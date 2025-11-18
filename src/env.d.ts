4cae5eb6bbf21d9fdcbd37d54b89d1123123d66f
DFedizko <fedizkopedro@gmail.com>
2025-11-17 22:59:07 -0300

    adding .env typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,13 @@ declare namespace App {
 		variant: Promise<string> | string;
 	}
 }
+
+interface ImportMetaEnv {
+	readonly POSTHOG_KEY: string;
+	readonly POSTHOG_HOST: string;
+}
+
+// biome-ignore lint/correctness/noUnusedVariables: import meta env typing
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
